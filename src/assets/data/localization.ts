280ed7d1080424ba6d0892728b5eea23f291a935
Dina <xuong.banh@jellyfishhr.com>
2018-05-22 15:02:15 +0900

    Create category page

--- src/assets/data/localization.ts
+++ src/assets/data/localization.ts
@@ -1,41 +1,105 @@
 export default {
   C0001: {
-   EN: 'Cold Ramen',
-   JP: '冷たい麺'
+    EN: {
+      name: 'Cold Ramen',
+      color: 'violet',
+      img: '../../assets/imgs/category/coldRamenSqr.png'
+    },  
+    JP: {
+      name: '冷たい麺',
+      color: 'violet',
+      img: '../../assets/imgs/category/coldRamenSqr.png'
+    }
   }, 
 
   C0002: {
-    EN: 'Hot Ramen',
-    JP: '温かい麺'
+    EN: {
+      name: 'Hot Ramen',
+      color: 'red',
+      img: '../../assets/imgs/category/coldRamenSqr.png'
+    },  
+    JP: {
+      name: '温かい麺',
+      color: 'red',
+      img: '../../assets/imgs/category/coldRamenSqr.png'
+    }
   }, 
 
   C0003: {
-    EN: 'Food',
-    JP: 'ご飯物'
+    EN: {
+      name: 'Food',
+      color: 'green',
+      img: '../../assets/imgs/category/coldRamenSqr.png'
+    },  
+    JP: {
+      name: 'ご飯物',
+      color: 'green',
+      img: '../../assets/imgs/category/coldRamenSqr.png'
+    }
   }, 
 
   C0004: {
-    EN: 'Summer Only',
-    JP: '夏季限定'
+    EN: {
+      name: 'Summer Only',
+      color: 'orange',
+      img: '../../assets/imgs/category/coldRamenSqr.png'
+    },  
+    JP: {
+      name: '夏季限定',
+      color: 'orange',
+      img: '../../assets/imgs/category/coldRamenSqr.png'
+    }
   }, 
 
   C0005: {
-    EN: 'Ice Shaved',
-    JP: 'かき氷（夏季限定）'
+    EN: {
+      name: 'Ice Shaved',
+      color: 'blue',
+      img: '../../assets/imgs/category/coldRamenSqr.png'
+    },  
+    JP: {
+      name: 'かき氷（夏季限定）',
+      color: 'blue',
+      img: '../../assets/imgs/category/coldRamenSqr.png'
+    }
   }, 
 
   C0006: {
-    EN: 'Drink',
-    JP: 'お飲み物'
+    EN: {
+      name: 'Drink',
+      color: 'yellow',
+      img: '../../assets/imgs/category/coldRamenSqr.png'
+    },  
+    JP: {
+      name: 'お飲み物',
+      color: 'yellow',
+      img: '../../assets/imgs/category/coldRamenSqr.png'
+    }
   }, 
 
   C0007: {
-    EN: 'A La Carte',
-    JP: '一品料理'
+    EN: {
+      name: 'A La Carte',
+      color: 'amber',
+      img: '../../assets/imgs/category/coldRamenSqr.png'
+    },  
+    JP: {
+      name: '一品料理',
+      color: 'amber',
+      img: '../../assets/imgs/category/coldRamenSqr.png'
+    }
   }, 
 
   C0008: {
-    EN: 'Take Away',
-    JP: 'お持ち帰り（生そば）'
-  }
+    EN: {
+      name: 'Take Away',
+      color: 'brown',
+      img: '../../assets/imgs/category/coldRamenSqr.png'
+    },  
+    JP: {
+      name: 'お持ち帰り（生そば）',
+      color: 'brown',
+      img: '../../assets/imgs/category/coldRamenSqr.png'
+    }
+  }, 
 }
